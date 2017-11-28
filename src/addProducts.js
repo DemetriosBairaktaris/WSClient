0087e0bf25f1d0c709fe879a0f7c672d3fbbb37a
dbairaktaris1 <dbairaktaris1@luc.edu>
2017-11-28 01:01:48 -0600

    able to add products and update partner

---
+++ src/addProducts.js
@@ -0,0 +1,194 @@
+addProducts = {
+	initialize: function( links ) {
+		$( "main" )
+			.empty();
+		$( "main" )
+			.append( getAddItemForm() )
+		addLinks( links );
+	}
+}
+
+function handleDelete( link ) {
+	var url = hostName + link.uri;
+	var verb = "DELETE";
+	var accepts = "application/luc.partners+json";
+	console.log( url );
+	$.ajax( {
+		accepts: accepts,
+		type: verb,
+		url: url,
+		success: function( data ) {
+			$( "main" )
+				.empty();
+			$( "#current-user" )
+				.remove();
+			$( "main" )
+				.append( getSuccessMsg( "Deleted Your Account" ) );
+		},
+		error: function( data ) {
+			$( "main" )
+				.append( getFailureMsg( "Could Not Delete" ) )
+		}
+	} );
+};
+
+function handlePut( link ) {
+	var inputValue = $( "#" + link.rel.replace( ".", "" )
+			.replace( " ", "" ) + "_input" )
+		.val();
+	var url = hostName + link.uri;
+	url = url.split( "{" )[ 0 ] + inputValue;
+	console.log( url );
+	var verb = "PUT";
+	var accepts = "application/luc.partners+json";
+	console.log( url );
+	$.ajax( {
+		accepts: accepts,
+		type: verb,
+		url: url,
+		success: function( data ) {},
+		error: function( data ) {
+			$( "main" )
+				.append( getFailureMsg( "Could Not Update" ) )
+		}
+	} );
+};
+
+function collectFromProductForm() {
+	var data = {}
+	var objectName = "Product";
+	data[ objectName ] = {}
+	var $inputs = $( "#add-product-form * input" );
+	for ( var i = 0; i < $inputs.length; i++ ) {
+		data[ objectName ][ $inputs[ i ].id ] = String( $( $inputs[ i ] )
+			.val() );
+	}
+	data[ objectName ][ "companyUserName" ] = currentUser;
+	console.log( data );
+	return JSON.stringify( data );
+}
+
+function handleAddProduct( link ) {
+	var url = hostName + link.uri;
+	var dataType = "json";
+	var verb = "POST";
+	var contentType = "application/luc.products+json";
+	var accepts = "application/json";
+	console.log( url );
+	console.log( collectFromProductForm() );
+	$.ajax( {
+		jsonp: false,
+		dataType: dataType,
+		accepts: accepts,
+		contentType: contentType,
+		type: verb,
+		url: url,
+		data: collectFromProductForm(),
+		success: function( data ) {
+			console.log( data );
+			$( "main" )
+				.prepend( getSuccessMsg( "Created Product" ) );
+			setTimeout( function() {
+				$( ".success-message" )
+					.fadeOut( 3000, function() {
+						$( ".success-message" )
+							.remove();
+					} );
+			}, 3000 );
+		},
+		error: function( data, status, err ) {
+			if ( Number( data.status ) == 200 ) {
+				//ask me later
+			} else {}
+		}
+	} );
+};
+/**
+0 {action: "DELETE", contentType: "none", rel: "Delete account", uri: "/partners/sjobs@apple.com"}
+1 {action: "PUT", contentType: "none", rel: "Update name.", uri: "/partners/sjobs@apple.com/{newName}"}
+2 {action: "PUT", contentType: "none", rel: "Update address.", uri: "/partners/sjobs@apple.com/{newAddress}"}
+3 {action: "PUT", contentType: "none", rel: "Update phone.", uri: "/partners/sjobs@apple.com/{newPhone}"}
+4 {action: "POST", contentType: "application/luc.products+xml, application/luc.products+json", rel: "add products", uri: "/products"}
+**/
+function gimmeGenericForm( link ) {
+	var html = "";
+	if ( link.action == "DELETE" ) {
+		html = `
+		<legend>` + link.rel.replace( ".", "" ) +
+			`</legend>
+		<div class='pure-u-5-5'>
+		<button id=` + link.rel.replace( ".",
+				"" )
+			.replace( " ", "" ) +
+			` class="pure-button pure-button-inverted">Submit</button>
+		</div>
+	`;
+		$( "#add-product-form" )
+			.append( html );
+		$( "#" + link.rel.replace( ".", "" )
+				.replace( " ", "" ) )
+			.on( "click", function() {
+				console.log( "deleting event..." );
+				handleDelete( link );
+			} );
+	} else {
+		html = `
+		<legend>` + link.rel.replace( ".", "" ) +
+			`</legend>
+		<div class='pure-u-5-5'>
+		<input id = ` + link.rel.replace(
+				".", "" )
+			.replace( " ", "" ) + "_input" + ` type="text" placeholder=` + link.rel +
+			`>
+		<button id=` + link.rel.replace( ".", "" )
+			.replace( " ", "" ) +
+			` class="pure-button pure-button-inverted">Submit</button>
+		</div>
+	`;
+		$( "#add-product-form" )
+			.append( html );
+		$( "#" + link.rel.replace( ".", "" )
+				.replace( " ", "" ) )
+			.on( "click", function() {
+				handlePut( link );
+			} );
+	}
+}
+
+function addLinks( links ) {
+	links.forEach( function( link ) {
+		console.log( link.action );
+		if ( link.contentType == "none" ) {
+			gimmeGenericForm( link )
+		} else if ( true ) {
+			$( "#product-submit-button" )
+				.on( "click", function() {
+					handleAddProduct( link );
+				} );
+		}
+	} );
+};
+
+function getAddItemForm() {
+	var html =
+		`
+
+		<div id='add-product-form' class='pure-form pure-u-4-5 island center'>
+			<fieldset>
+        		<legend class="main-color-header">Add a product...</legend>
+        			<div class= "pure-u-3-5">
+        			 <input id = "desc" type="text" placeholder="Product Name">
+       				 <input id = "cost" type="text" placeholder="Cost">
+       				 <input id = 'stock' type="number" placeholder= "Stock">
+       				 <input id = "name" type="text" placeholder="Description">
+       				</div>
+       				<div class='pure-u-5-5'>
+					    <button id="product-submit-button"class="pure-button">Add Product</button>
+					  </div>
+       		</fieldset>
+		</div>
+
+
+	`;
+	return html;
+};
--- src/functions.js
+++ src/functions.js
@@ -1,101 +1,104 @@
-var hostName = "http://localhost:8081";
+hostName = "http://localhost:8081";
 
 function getRegisterHtml() {
-
     var html = getCustomerSignUpForm();
     return html;
 }
 
-function getSuccessMsg(msg){
-
-	var html = `
-		<div class="pure-u-4-5 island message success-message center ">	
-			<p>Success: ` + msg + `</p>
-		</div>
-	` ; 
-	return html ;
+function getSuccessMsg( msg ) {
+    var html =
+        `
+        <div class="pure-u-4-5 island message success-message center "> 
+            <p>Success: ` +
+        msg + `</p>
+        </div>
+    `;
+    return html;
 }
 
-function getFailureMsg(msg){
-
-	var html = `
-		<div class="pure-u-4-5 island message failure-message center ">	
-			<p>Failure: ` + msg + `</p>
-		</div>
-	` ; 
-	return html ;
+function getFailureMsg( msg ) {
+    var html =
+        `
+        <div class="pure-u-4-5 island message failure-message center "> 
+            <p>Failure: ` +
+        msg + `</p>
+        </div>
+    `;
+    return html;
 }
 
 function getCustomerSignUpForm() {
-    var html = `
-	<div id="register-form" class="pure-form pure-u-4-5 center island">
-    			<fieldset>
-        		<legend class="main-color-header">Register New Customer Account</legend>
-        			<div class= "pure-u-3-5">
-        			 <input id = "userName" type="email" placeholder="Email">
-       				 <!--<input type="password" placeholder="Password" >-->
-       				 <input id = "firstName" type="text" placeholder="First Name">
-       				 <input id = "lastName" type="text" placeholder="Last Name">
-       				 <input id = "address" type="text" placeholder="Address">
-       				 <input id = "phone" type="text" placeholder="Phone">
-       				
-       				
-       				 
-       				 	 <input id="cardNumber" type="text" placeholder="Credit Card #">
-	       				 <input id="cardName" type="text" placeholder="Credit Card Type">
-	       				 <input id="cvv" type="text" placeholder="CVV">
-	       				 <input id="expiration" type="date" placeholder="Expiration">
-       				 </div>
+    var html =
+        `
+    <div id="register-form" class="pure-form pure-u-4-5 center island">
+                <fieldset>
+                <legend class="main-color-header">Register New Customer Account</legend>
+                    <div class= "pure-u-3-5">
+                     <input id = "userName" type="email" placeholder="Email">
+                     <input id = "password" type="password" placeholder="Password">
+                     <input id = "firstName" type="text" placeholder="First Name">
+                     <input id = "lastName" type="text" placeholder="Last Name">
+                     <input id = "address" type="text" placeholder="Address">
+                     <input id = "phone" type="text" placeholder="Phone">
+                    
+                    
+                     
+                         <input id="cardNumber" type="text" placeholder="Credit Card #">
+                         <input id="cardName" type="text" placeholder="Credit Card Type">
+                         <input id="cvv" type="text" placeholder="CVV">
+                         <input id="expiration" type="date" placeholder="Expiration">
+                     </div>
 
-       				 </fieldset>
+                     </fieldset>
 
-       				 <div class="pure-button-group pure-u-3-5" role="group">
-					    <button id="customer-button"class="pure-button pure-button-active">Customer</button>
-					    <button id = "partner-button" class="pure-button">Partner</button>
-					</div>
-					 <h6>Register as customer or partner</h6>
-					<div class="pure-u-2-5">
-						<button id="register-submit-button" type="submit" class="pure-button pure-button-inverted">Register</button>
-						<a onclick="location.reload()">Sign In</a>
-					</div>
-					
-    			
-			</div> `
+                     <div class="pure-button-group pure-u-3-5" role="group">
+                        <button id="customer-button"class="pure-button pure-button-active">Customer</button>
+                        <button id = "partner-button" class="pure-button">Partner</button>
+                    </div>
+                     <h6>Register as customer or partner</h6>
+                    <div class="pure-u-2-5">
+                        <button id="register-submit-button" type="submit" class="pure-button pure-button-inverted">Register</button>
+                        <a onclick="location.reload()">Sign In</a>
+                    </div>
+                    
+                
+            </div> `
     return html;
 }
 
 function getPartnerSignUpForm() {
-    var html = `
-	<div id="register-form"class="pure-form pure-form-stacked pure-u-4-5 center island">
-    			<fieldset>
-        		<legend class="main-color-header">Register New Partner Account</legend>
-        			<div class= "pure-u-3-5">
-        			 <input id = "userName" type="email" placeholder="Email" required>
-       				 <!--<input type="password" placeholder="Password" >-->
-       				 <input  id = "name" type="text" placeholder="Company Name" required >
-       				 <input  id = "address" type="text" placeholder="Address" required >
-       				 <input  id = "phone" type="text" placeholder="Phone" required>
-       				 </div>
+    var html =
+        `
+    <div id="register-form"class="pure-form pure-form-stacked pure-u-4-5 center island">
+                <fieldset>
+                <legend class="main-color-header">Register New Partner Account</legend>
+                    <div class= "pure-u-3-5">
+                     <input id = "userName" type="email" placeholder="Email"  required>
+                     <input id="password" type="password" placeholder="Password"  >
+                     <input  id = "name" type="text" placeholder="Company Name"  required >
+                     <input  id = "address" type="text" placeholder="Address"  required >
+                     <input  id = "phone" type="text" placeholder="Phone" required>
+                     </div>
 
-       				 </fieldset>
+                     </fieldset>
 
-       				 <div class="pure-button-group pure-u-3-5" role="group">
-					    <button id="customer-button"class="pure-button ">Customer</button>
-					    <button id = "partner-button" class="pure-button pure-button-active">Partner</button>
-					</div>
-					 <h6>Register as customer or partner</h6>
-					<div class="pure-u-2-5">
-						<button id="register-submit-button" type="submit" class="pure-button pure-button-inverted">Register</button>
-						<a onclick="location.reload()">Sign In</a>
-					</div>
-    			
-			</div> `
+                     <div class="pure-button-group pure-u-3-5" role="group">
+                        <button id="customer-button"class="pure-button ">Customer</button>
+                        <button id = "partner-button" class="pure-button pure-button-active">Partner</button>
+                    </div>
+                     <h6>Register as customer or partner</h6>
+                    <div class="pure-u-2-5">
+                        <button id="register-submit-button" type="submit" class="pure-button pure-button-inverted">Register</button>
+                        <a onclick="location.reload()">Sign In</a>
+                    </div>
+                
+            </div> `
     return html;
 }
 
 function registerTypeIsCustomer() {
-    var $button = $(".pure-button-active");
-    if ($button.attr("id") == 'customer-button') {
+    var $button = $( ".pure-button-active" );
+    if ( $button.attr( "id" ) == 'customer-button' ) {
         return true;
     } else {
         return false;
@@ -103,120 +106,117 @@ function registerTypeIsCustomer() {
 }
 
 function applyRegisterLogic() {
-
-    $buttonGroup = $("#register-form .pure-button-group");
+    $buttonGroup = $( "#register-form .pure-button-group" );
     $buttonGroup.children()
-        .on("click", function() {
+        .on( "click", function() {
             $buttonGroup.children()
-                .removeClass("pure-button-active");
-            $(this)
-                .addClass("pure-button-active");
-
-            $main = $("main");
+                .removeClass( "pure-button-active" );
+            $( this )
+                .addClass( "pure-button-active" );
+            $main = $( "main" );
             var isCustomerRegistration = registerTypeIsCustomer();
             $main.empty();
-            if (isCustomerRegistration) {
-
-                $main.append(getCustomerSignUpForm());
+            if ( isCustomerRegistration ) {
+                $main.append( getCustomerSignUpForm() );
             } else {
-
-                $main.append(getPartnerSignUpForm());
+                $main.append( getPartnerSignUpForm() );
             }
             applyRegisterLogic(); //psudo-recursion, coooooool
-        });
-
-    $("#register-submit-button")
-        .on("click", function() {
-        	$(".message").remove() ; 
-            if (registerTypeIsCustomer()) {
+        } );
+    $( "#register-submit-button" )
+        .on( "click", function() {
+            $( ".message" )
+                .remove();
+            if ( registerTypeIsCustomer() ) {
                 handleCustomerRegister();
             } else {
                 handlePartnerRegister();
             }
-        });
+        } );
 }
 
-
-function collectFromForm(objectName) {
+function collectFromForm( objectName ) {
     var data = {}
-    data[objectName] = {}
-    var $inputs = $("#register-form * input");
-    for (var i = 0; i < $inputs.length; i++) {
-        data[objectName][$inputs[i].id] = String($($inputs[i])
-            .val());
-        console.log(JSON.stringify(data[objectName]));
+    data[ objectName ] = {}
+    var $inputs = $( "#register-form * input" );
+    for ( var i = 0; i < $inputs.length; i++ ) {
+        data[ objectName ][ $inputs[ i ].id ] = String( $( $inputs[ i ] )
+            .val() );
+        console.log( JSON.stringify( data[ objectName ] ) );
     }
-    return JSON.stringify(data);
+    return JSON.stringify( data );
 };
 
 function handleCustomerRegister() {
-	var url = hostName + "/customers/";
+    console.log( collectFromForm( "Customer" ) );
+    var url = hostName + "/customers/";
     var dataType = "json";
     var verb = "POST";
     var contentType = "application/json";
     var accepts = "application/json";
-
-     $.ajax({
-     	jsonp: false,
-     	dataType: dataType,
+    $.ajax( {
+        jsonp: false,
+        dataType: dataType,
         accepts: accepts,
         contentType: contentType,
         type: verb,
         url: url,
-        data: collectFromForm("Customer"),
-        success: function(data) {
-        	console.log(data) ;
-            registerSuccess(data.Customer.userName + " has been created") ; 
+        data: collectFromForm( "Customer" ),
+        success: function( data ) {
+            console.log( data );
+            registerSuccess( data.Customer.userName + " has been created" );
         },
-        error: function(data,status,err) {
-        	if(Number(data.status) < 300){
-        		registerSuccess("Customer has been created") ; 
-        		//ask me later
-        	}
-        	else{
-	        	console.log(data) ; 
-	        	console.log(status) ;
-	        	console.log(err) ; 
-	            registerFailure("Could not create customer");
-       		 }
+        error: function( data, status, err ) {
+            if ( Number( data.status ) == 200 ) {
+                registerSuccess( "Customer has been created" );
+                //ask me later
+            } else {
+                console.log( data );
+                console.log( status );
+                console.log( err );
+                registerFailure( "Could not create customer" );
+            }
         }
-       
-    });
-
+    } );
 };
 
 function handlePartnerRegister() {
-
+    console.log( collectFromForm( "Partner" ) );
     var url = hostName + "/partners";
     var dataType = "json";
     var verb = "POST";
-    var contentType = "application/json";
-    var accepts = "application/json";
-    $.ajax({
+    var contentType = "application/luc.partners+json";
+    var accepts = "application/luc.partners+json";
+    $.ajax( {
         accepts: accepts,
         contentType: contentType,
         type: verb,
         url: url,
-        data: collectFromForm("Partner"),
-        success: function(data) {
-            registerSuccess( data.Partner.userName + " has been created");
+        data: collectFromForm( "Partner" ),
+        success: function( data ) {
+            console.log( "///////////" );
+            console.log( data );
+            $( "#current-user" )
+                .text( data.Partner.userName );
+            currentUser = data.Partner.userName;
+            addProducts.initialize( data.Partner.link ); //need to see
+            //registerSuccess( data.Partner.userName + " has been created");
         },
-        error: function(data) {
-            registerFailure( "Could not create Partner");
+        error: function( data ) {
+            registerFailure( "Could not create Partner" );
         },
         dataType: dataType
-    });
+    } );
 };
 
-function registerSuccess(msg){
-	var $main = $("main") ;
-	$main.prepend(getSuccessMsg(msg)) ; 
+function registerSuccess( msg ) {
+    var $main = $( "main" );
+    $main.prepend( getSuccessMsg( msg ) );
 };
-function registerFailure(msg){
-	var $main = $("main") ;
-	$main.prepend(getFailureMsg(msg)) ; 
-}; 
 
-function applySignInLogic() {
+function registerFailure( msg ) {
+    var $main = $( "main" );
+    $main.prepend( getFailureMsg( msg ) );
+};
 
-}
+function applySignInLogic() {}
